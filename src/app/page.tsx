2bcd39c5e6b6fb526b88d8ed160f037e2c86ccf5
Brad Garropy <bradgarropy@gmail.com>
2025-04-09 21:46:47 -0500

    load movies from search params.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,28 @@
-const Home = () => {
+import Search from "~/components/Search"
+import {movieClient} from "~/utils/movies"
+
+type HomeProps = {
+    searchParams: Promise<{search?: string}>
+}
+
+const Home = async (props: HomeProps) => {
+    const searchParams = await props.searchParams
+    const search = searchParams.search ?? ""
+    const movies = await movieClient.getMovies({search})
+
     return (
         <div className="grid grid-rows-[auto_1fr_auto] gap-4 min-h-screen">
             <header className="bg-slate-900 h-16 m-2 px-4 py-2 rounded-lg text-white"></header>
 
             <main className="grid grid-cols-[3fr_1fr] gap-4 mx-2">
-                <section className="border border-slate-900">main</section>
+                <section className="border border-slate-900">
+                    <Search />
+
+                    {movies.length > 0 ? (
+                        <pre>{JSON.stringify(movies, null, 2)}</pre>
+                    ) : null}
+                </section>
+
                 <section className="border border-slate-900">sidebar</section>
             </main>
 
